0b9e26d53475597cd7d856fbb41627b91455433f
Dawenster <dstwen@gmail.com>
2013-03-07 12:12:40 -0800

    Created Student Class inheriting from ActiveRecord and added name and age methods

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+
+	def name
+		first_name + ' ' + last_name
+	end
+
+	def age
+		now = Time.now.utc.to_date
+		age = now.year - birthday.year
+		if birthday.yday > now.yday
+			age -= 1
+		end
+		age
+	end
+end
